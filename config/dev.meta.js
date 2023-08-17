3fdd13b3951f6de69463316c71b58b5268243a00
徐冰 <38274876+forestsheep911@users.noreply.github.com>
2023-08-17 14:15:18 +0800

    gogogo

--- config/dev.meta.js
+++ config/dev.meta.js
@@ -3,7 +3,7 @@ const { resolve } = require('path')
 module.exports = {
   name: `${pj.name}`,
   namespace: `https://github.com/${pj.author}/monkey-${pj.name}`,
-  match: ['https://*'],
-  grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener'],
+  match: ['https://tongji.baidu.com/*'],
+  grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener', 'GM_xmlhttpRequest'],
   require: [`file://${resolve(__dirname, '../dist/dev').replaceAll('\\', '/')}/${pj.name}.script.js`],
 }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,36 @@
 const app = () => {
-  console.log('monkey jumping on the bed.')
+  console.log('monkey jumping on the tongji')
+  setTimeout(() => {
+    const elementLink = document.querySelectorAll('.access_page a') as NodeListOf<HTMLAnchorElement>
+    console.log(elementLink[0].href)
+    if (!elementLink || elementLink.length === 0) {
+      return
+    }
+    elementLink.forEach((e) => {
+      e.addEventListener(
+        'mouseenter',
+        () => {
+          console.log('mouse enter')
+          // access page by fetch and get the response
+          GM_xmlhttpRequest({
+            method: 'GET',
+            url: e.href,
+            onload: function (response) {
+              // console.log(response.responseXML)
+              const document = response.responseText
+              const regExp = /<title>(.*)<\/title>/
+              const ed = regExp.exec(document)
+              if (ed && ed.length >= 1) {
+                console.log(ed[1])
+                e.textContent = ed[1]
+              }
+            },
+          })
+        },
+        { once: true },
+      )
+    })
+  }, 3000)
 }
 
 export default app
